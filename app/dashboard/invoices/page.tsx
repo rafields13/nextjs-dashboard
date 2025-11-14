ffbb2e1ea7224d72a29ed5ccaaa2ccee96de4bdb
Rafael Marliere de Oliveira <rafaelmarliere033@gmail.com>
2025-11-14 15:49:50 -0300

    remove some comments to pagination works and remove logs and notes from code

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -17,7 +17,6 @@ export default async function Page(props: {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
-  console.log(totalPages);
 
   return (
     <div className="w-full">
